11aa9bb8b6c2a237d928b5b423d93348f063e54e
bruno.nogareda <brunonogareda@gmail.com>
2017-03-11 19:20:21 +0100

    Initial Commit

---
+++ servo.py
@@ -0,0 +1,33 @@
+from RPIO import PWM	#importamos a libreria para PWM
+import time
+import sys
+
+pin_servo1 = 2
+pin_servo2 = 3
+
+#Esta funcion calcula o tempo de activacion do pulso en us en funcion do angulo que se lle pasa.
+def angleToTime(angulo):
+	if angulo<=90 and angulo>=-90:
+		return (((90.0+angulo)*(50.0/9.0))+1000)
+	else:
+		return 1500
+
+#Miramos se existe un argumento de entrada utilizamolo como angulo
+angle1 = 0
+angle2 = 0
+if len(sys.argv)>2:
+	angle1 = int(sys.argv[1])
+	angle2 = int(sys.argv[2])
+
+pulso1 = angleToTime(angle1)
+pulso2 = angleToTime(angle2)
+
+print 'Pulso1: '+ repr(pulso1) +', Pulso2: '+ repr(pulso2)
+servo=PWM.Servo()				#Iniciamos a libreria para o servo
+servo.set_servo(pin_servo1,1500)		#Centramos o servo
+servo.set_servo(pin_servo2,1500)
+time.sleep(1)
+servo.set_servo(pin_servo1,pulso1)
+servo.set_servo(pin_servo2,pulso2)
+servo.stop_servo(pin_servo1)
+servo.stop_servo(pin_servo2)
