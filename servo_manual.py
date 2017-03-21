7b6cb371db442c2cc24c9dc5408d34f2de6f7b8d
bruno.nogareda <brunonogareda@gmail.com>
2017-03-21 10:43:11 +0100

    Añadido función servo manual

---
+++ servo_manual.py
@@ -0,0 +1,91 @@
+import RPi.GPIO as GPIO
+import math
+import xbox
+
+GPIO_LED_GREEN  = 23
+GPIO_LED_RED    = 22
+GPIO_LED_YELLOW = 27
+GPIO_LED_BLUE   = 17
+
+GPIO_SERVO1  = 2
+GPIO_SERVO2  = 3
+
+
+GPIO.setmode(GPIO.BCM)
+GPIO.setwarnings(False)
+
+GPIO.setup(GPIO_LED_GREEN, GPIO.OUT)
+GPIO.setup(GPIO_LED_RED, GPIO.OUT)
+GPIO.setup(GPIO_LED_YELLOW, GPIO.OUT)
+GPIO.setup(GPIO_LED_BLUE, GPIO.OUT)
+GPIO.setup(GPIO_SERVO1, GPIO.OUT)
+GPIO.setup(GPIO_SERVO2, GPIO.OUT)
+
+
+
+def updateServo(pwm, angle):
+    duty = float(angle) / 10.0 + 2.5
+    pwm.ChangeDutyCycle(duty)
+
+def angleFromCoords(x,y):
+    angle = 0.0
+    if x==0.0 and y==0.0:
+        angle = 90.0
+    elif x>=0.0 and y>=0.0:
+        # first quadrant
+        angle = math.degrees(math.atan(y/x)) if x!=0.0 else 90.0
+    elif x<0.0 and y>=0.0:
+        # second quadrant
+        angle = math.degrees(math.atan(y/x))
+        angle += 180.0
+    elif x<0.0 and y<0.0:
+        # third quadrant
+        angle = math.degrees(math.atan(y/x))
+        angle += 180.0
+    elif x>=0.0 and y<0.0:
+        # third quadrant
+        angle = math.degrees(math.atan(y/x)) if x!=0.0 else -90.0
+        angle += 360.0
+    return angle
+
+if __name__ == '__main__':
+    joy = xbox.Joystick()
+    pwm1 = GPIO.PWM(GPIO_SERVO1, 100)
+    pwm2 = GPIO.PWM(GPIO_SERVO2, 100)
+    pwm1.start(5)
+    pwm2.start(5)
+
+    while not joy.Back():
+
+        # LEDs
+        led_state_green  = GPIO.HIGH if joy.A() else GPIO.LOW
+        led_state_red    = GPIO.HIGH if joy.B() else GPIO.LOW
+        led_state_yellow = GPIO.HIGH if joy.Y() else GPIO.LOW
+        led_state_blue   = GPIO.HIGH if joy.X() else GPIO.LOW
+
+        GPIO.output(GPIO_LED_GREEN, led_state_green)
+        GPIO.output(GPIO_LED_RED, led_state_red)
+        GPIO.output(GPIO_LED_YELLOW, led_state_yellow)
+        GPIO.output(GPIO_LED_BLUE, led_state_blue)
+
+        # Servo
+        x1, y1 = joy.leftStick()
+        x2, y2 = joy.rightStick()
+        angle1 = angleFromCoords(x1,y1)
+        angle2 = angleFromCoords(x,y2)
+        if angle1 > 180 and angle1 < 270:
+            angle1 = 180
+        elif angle1 >= 270:
+            angle1 = 0
+        updateServo(pwm1, angle1)
+
+        if angle2 > 180 and angle2 < 270:
+            angle2 = 180
+        elif angle2 >= 270:
+            angle2 = 0
+        updateServo(pwm2, angle2)
+
+
+    joy.close()
+    pwm1.stop()
+    pwm2.stop()
