801393412d9a3d65d349f4a366436b3c7ffaf93b
bruno.nogareda <brunonogareda@gmail.com>
2017-03-22 20:47:13 +0100

    Mejoras de control básico para servo.

--- servo.py
+++ servo.py
@@ -4,8 +4,14 @@ import sys
 
 SERVO_ERR = 3
 
-pin_servo1 = 2
-pin_servo2 = 3
+GPIO_SERVO1 = 2
+GPIO_SERVO2 = 3
+
+GPIO.setmode(GPIO.BCM)
+GPIO.setwarnings(False)
+
+GPIO.setup(GPIO_SERVO1, GPIO.OUT)
+GPIO.setup(GPIO_SERVO2, GPIO.OUT)
 
 def updateServo(pwm, angle):
 	if angle<0 or angle>180:
@@ -14,22 +20,25 @@ def updateServo(pwm, angle):
 	pwm.ChangeDutyCycle(duty+SERVO_ERR)
 
 #Miramos se existe un argumento de entrada utilizamolo como angulo
-angle1 = 0
-angle2 = 0
+angle1 = 90
+angle2 = 90
 if len(sys.argv)>2:
 	angle1 = int(sys.argv[1])
 	angle2 = int(sys.argv[2])
 
-pulso1 = angleToTime(angle1)
-pulso2 = angleToTime(angle2)
+pwm1 = GPIO.PWM(GPIO_SERVO1, 100)
+pwm2 = GPIO.PWM(GPIO_SERVO2, 100)
+pwm1.start(5)
+pwm2.start(5)
 
-print 'Pulso1: '+ repr(pulso1) +', Pulso2: '+ repr(pulso2)
-servo=PWM.Servo()				#Iniciamos a libreria para o servo
-servo.set_servo(pin_servo1,1500)		#Centramos o servo
-servo.set_servo(pin_servo2,1500)
+updateServo(pw1, angle1)
+updateServo(pw2, angle2)
 time.sleep(1)
-servo.set_servo(pin_servo1,pulso1)
-servo.set_servo(pin_servo2,pulso2)
-time.sleep(1)
-#servo.stop_servo(pin_servo1)
-#servo.stop_servo(pin_servo2)
+print "Pulse [Enter] para terminar."
+s = sys.stdin.read(1)
+
+updateServo(pwm1, 90)
+updateServo(pwm2, 90)
+time.sleep(.5)
+pwm1.stop()
+pwm2.stop()
